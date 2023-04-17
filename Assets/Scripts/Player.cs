47a25654a47dd96a06fde9f798bfe13bd941499c
Katie Alberda <112493386+Alberda-Katie-Student@users.noreply.github.com>
2023-04-17 09:49:44 -0400

    Monday 4/17/23

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,6 +16,13 @@ Vector2 rawInput;
 
 Vector2 minBounds;
 Vector2 maxBounds;
+Shooter shooter;
+
+void Awake() 
+{
+    shooter = GetComponent<Shooter>();
+    
+}
 void Start() {
     {
         InitBounds();
@@ -44,6 +51,13 @@ void Start() {
     void OnMove(InputValue value)
     {
         rawInput = value.Get<Vector2>();
-        Debug.Log(rawInput);
+    }
+
+    void OnFire(InputValue value)
+    {
+        if(shooter !=null)
+        {
+            shooter.isFiring = value.isPressed;
+        }
     }
 }
